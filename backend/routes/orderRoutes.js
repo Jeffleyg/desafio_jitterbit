2897d6bcc29d5a7bbc43ab7de657fc32bced65c0
jeffleyg <jeffleygarcon007@gmail.com>
2026-03-08 17:03:34 -0300

    feat: implement os routes de get all, get by id, update e remove

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -56,6 +56,93 @@ router.post('/', validatePedido, orderController.criar);
  *               items:
  *                 $ref: '#/components/schemas/Order'
  */
+router.get('/', orderController.getAll);
+
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   get:
+ *     summary: Obter pedido por ID
+ *     tags: [Orders]
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do pedido
+ *     responses:
+ *       200:
+ *         description: Detalhes do pedido
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Order'
+ *       404:
+ *         description: Pedido não encontrado
+ */
+router.get('/:id', orderController.getById);
+
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   put:
+ *     summary: Atualizar pedido
+ *     tags: [Orders]
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do pedido
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               value:
+ *                 type: number
+ *               items:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/Item'
+ *     responses:
+ *       200:
+ *         description: Pedido atualizado com sucesso
+ *       404:
+ *         description: Pedido não encontrado
+ */
+router.put('/:id', orderController.atualizar);
+
+/**
+ * @swagger
+ * /api/orders/{id}:
+ *   delete:
+ *     summary: Deletar pedido
+ *     tags: [Orders]
+ *     security:
+ *       - BearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: ID do pedido
+ *     responses:
+ *       200:
+ *         description: Pedido deletado com sucesso
+ *       404:
+ *         description: Pedido não encontrado
+ */
+router.delete('/:id', orderController.remove);
 
 
 module.exports = router;
